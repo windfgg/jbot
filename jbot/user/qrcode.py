ef2d5e7c535055456711fb03a226947e31239721
FengG <windfgg@gmail.com>
2022-04-12 09:33:37 +0800

    Update QrCode User

--- jbot/user/qrcode.py
+++ jbot/user/qrcode.py
@@ -1,32 +1,32 @@
-import asyncio,qrcode
+import qrcode
 from telethon import events
+from .login import user
+from .. import QR_IMG_FILE
 
-# def creat_qr(text):
-#     '''实例化QRCode生成qr对象'''
-#     qr = qrcode.QRCode(
-#         version=1,
-#         error_correction=qrcode.constants.ERROR_CORRECT_H,
-#         box_size=10,
-#         border=4
-#     )
-#     qr.clear()
-#     # 传入数据
-#     qr.add_data(text)
-#     qr.make(fit=True)
-#     # 生成二维码
-#     img = qr.make_image()
-#     # 保存二维码
-#     img.save(QR_IMG_FILE)
+def creat_qr(text):
+    '''实例化QRCode生成qr对象'''
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4
+    )
+    qr.clear()
+    # 传入数据
+    qr.add_data(text)
+    qr.make(fit=True)
+    # 生成二维码
+    img = qr.make_image()
+    # 保存二维码
+    img.save(QR_IMG_FILE)
 
-# @jdbot.on(events.NewMessage(from_users=chat_id,pattern=r'^qrcode []$'))
-# async def user_login(event):
-#     try:
-#         user.connect()
-#         qr_login = await user.qr_login()
-#         creat_qr(qr_login.url)
-#         await jdbot.send_message(chat_id,'请使用TG扫描二维码以开启USER',file=QR_IMG_FILE)
-#         await jdbot.send_message(chat_id,qr_login.url)
-#         await qr_login.wait(timeout=100)
-#         await jdbot.send_message(chat_id,'恭喜您已登录成功,请修改 /set 将开启user 改为True 并重启机器人 /reboot')
-#     except Exception as e:
-#         await jdbot.send_message(chat_id,'登录失败\n'+str(e))
+@user.on(events.NewMessage(pattern=r'^qrcode [\s\S]*$', outgoing=True))
+async def user_login(event):
+    try:
+        num = event.raw_text.split(' ')
+        if isinstance(num, list) and len(num) == 2:
+            data = int(num[-1])
+            creat_qr(data)
+            await user.send_message(event.chat_id, '',file=QR_IMG_FILE)
+    except Exception as e:
+        await user.send_message(event.chat_id,'二维码生成失败\n'+str(e))
